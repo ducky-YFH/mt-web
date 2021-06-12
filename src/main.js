6f3909b5bea73067254e494d569131afe878d684
YFH <508766975@qq.com>
2021-06-12 22:38:37 +0800

    基本完成

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import "@/assets/style/main.less";
 import "@/assets/style/reset.less";
 
 import "@/common/header/index.less";
-import "@/common/side-nav/index.less";
 import "@/common/footer/index.less";
 
 import * as utils from '@/tools/utils.js';
---
+++ src/page/shop/index.js
@@ -0,0 +1,7 @@
+import 'baguettebox.js/dist/baguetteBox.min.css'
+import baguetteBox from 'baguettebox.js';
+import './index.less'
+
+// window.addEventListener('load', function() {
+  baguetteBox.run('.gallery');
+// });
